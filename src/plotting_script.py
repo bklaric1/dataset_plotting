818de01d327055dc1a03d497f42e62b1c19361a6
Benjamin Klaric <benjamin.klaric01@gmail.com>
2023-11-28 21:40:37 +0100

    tried to plot without success

--- src/plotting_script.py
+++ src/plotting_script.py
@@ -15,11 +15,12 @@ def load_dataset_and_visualize(dataset_filename):
         for ranking_problem in single_track.training_records:
             feature_tensors = [feat.variance_based_features for feat in ranking_problem.lane_candidates_feat]
 
-            #Loading is done, just figure out how to plot the dataset
+            left_points = LaneFeatureRecord.left_points[0]
+            right_points = LaneFeatureRecord.right_points[1]
 
             fig, ax = plt.subplots()
 
-            ax.plot(LaneFeatureRecord.left_points.__getattribute__, LaneFeatureRecord.right_points.__getattribute__, linewidth=2.0)
+            ax.plot(left_points, right_points, linewidth=2.0)
 
             ax.set(xlim=(0, 8), xticks=np.arange(1, 8),  ylim=(0, 8), yticks=np.arange(1, 8))
 
