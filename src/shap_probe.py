a3d170029c03a553df4e747fda819ccd8d678c73
bklaric <benjamin.klaric01@gmail.com>
2024-02-20 23:08:05 +0100

    added some code

--- src/shap_probe.py
+++ src/shap_probe.py
@@ -4,6 +4,7 @@ from typing import List
 from clc_training_data_creator.datasets_def import load_dataset, TrainingDataOneTrack, TrainingDataRecord, LaneFeatureRecord
 import shap
 import torch
+from torch import from_numpy as torch_t_from_numpy
 
 def load_dataset_shap(dataset_filename):
     print(f"Loading dataset ...")
@@ -20,16 +21,32 @@ def shap_plot(dataset):
                 feature_vector = lane_feature_record.variance_based_features
                 feature_vectors.append(feature_vector)
 
-    features_array = np.array(feature_vectors)
-    X = torch.tensor(features_array, dtype=torch.float32)
+    feature_pair = [torch_t_from_numpy(feature_vectors[0].astype(np.float32)), torch_t_from_numpy(feature_vectors[1].astype(np.float32))]
 
+    feature_pair = torch.unsqueeze(torch.stack(feature_pair).squeeze(), dim=0)
+    
     model = torch.load('C:\\Users\\Benjamin\\git\\clc_lane_rating_nn\\models\\clc_ranker.pth')
     model.eval()
 
-    explainer = shap.Explainer(model, X)
-    shap_values = explainer(X)
+    model.to("cpu")
 
-    shap.plots.waterfall(shap_values[0])
+    print(feature_pair)
+
+    feature_pair = torch.tensor(feature_pair, requires_grad=True)
+    out = model(feature_pair)
+    out.backward()
+
+    print(feature_pair.grad)
+
+    #print(f"Hallo: {feature_pair.shape}")
+
+    #print(model(feature_pair))
+    
+
+    #explainer = shap.Explainer(model, X)
+    #shap_values = explainer(X)
+
+    #shap.plots.waterfall(shap_values[0])
 
 dataset = load_dataset_shap('C:\\Users\\Benjamin\\Downloads\\dataset.pkl')
 shap_plot(dataset)
